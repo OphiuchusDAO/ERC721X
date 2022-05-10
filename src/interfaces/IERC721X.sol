07ba2cfbfa7c863027c02b5a1abf46344cd8891d
z80 <z80dev@mlnl.finance>
2022-05-10 18:18:53 -0400

    add InterfaceID constant for convenience

--- src/interfaces/IERC721X.sol
+++ src/interfaces/IERC721X.sol
@@ -3,6 +3,7 @@
 pragma solidity >=0.8.7 <0.9.0;
 
 interface IERC721X {
+    bytes4 constant InterfaceID = 0xefd00bbc;
     function originChainId() external view returns (uint32);
     function originAddress() external view returns (address);
     function setMinter(address _minter) external;
